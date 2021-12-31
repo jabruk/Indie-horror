3c674842d556728b8c39e8a4d120318d0d7a2097
jabruk <gurban780@gmail.com>
2021-12-31 15:19:22 +0600

    I limited the camera angle

--- Horror/Assets/Scripts/CameraController.cs
+++ Horror/Assets/Scripts/CameraController.cs
@@ -8,8 +8,13 @@ public class CameraController : MonoBehaviour
     private float mouseX;
     private float mouseY;
 
+    public float a;
+    public float b;
+
+    private float xRotatiuon;
+
     public Transform Player;
-    private float sensitivityMouse = 300f;
+    private float sensitivityMouse = 150f;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,9 +27,15 @@ public class CameraController : MonoBehaviour
         mouseX = Input.GetAxis("Mouse X") * sensitivityMouse * Time.deltaTime; 
         mouseY = Input.GetAxis("Mouse Y") * sensitivityMouse * Time.deltaTime;
 
+        a = mouseX;
+        b = mouseY;
+
+        xRotatiuon -= mouseY;
+        xRotatiuon = Mathf.Clamp(xRotatiuon, -90f, 90f);
+
         Player.Rotate(mouseX * new Vector3(0, 1, 0));
 
-        transform.Rotate(-mouseY * new Vector3(1, 0, 0));
-
+        //transform.Rotate(-mouseY * new Vector3(1, 0, 0));
+        transform.localRotation = Quaternion.Euler(xRotatiuon, 0f, 0f);
     }
 }
